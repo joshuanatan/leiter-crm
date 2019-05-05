0ddc5c61386ee4b8ad5fd879e8759293e8acbd42
joshuanatan <joshuanatan.jn@gmail.com>
2019-05-05 22:27:33 +0700

    pemisahan

---
+++ application/views/login/login-open.php
@@ -0,0 +1,3 @@
+<body class="animsition page-login-v2 layout-full page-dark">
+    <div class="page" data-animsition-in="fade-in" data-animsition-out="fade-out">
+            <div class="page-content">
---
+++ application/views/login/main-logo.php
@@ -0,0 +1,10 @@
+<div class="page-brand-info">
+    <div class="brand">
+        <img class="brand-img" src="../../assets/images/logo@2x.png" alt="...">
+        <h2 class="brand-text font-size-40">Remark</h2>
+    </div>
+    <p class="font-size-20">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.</p>
+</div>
+
+                
