833933166a97e9f9c11206e9085a47c239b82b39
joshuanatan <joshuanatan.jn@gmail.com>
2019-06-21 18:31:40 +0700

    rubah header, hilangin CRM

--- application/views/crm/request/category-header.php
+++ application/views/crm/request/category-header.php
@@ -1,5 +1,5 @@
 <div class="page-header">
-    <h1 class="page-title">CRM - REQUEST FOR QUOTATION (RFQ)</h1>
+    <h1 class="page-title">REQUEST FOR QUOTATION (RFQ)</h1>
     <br/>
     <ol class="breadcrumb breadcrumb-arrow">
         <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
