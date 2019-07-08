9416bb6fc2c5ef6a474b9429ba10ba831318c42c
wivinadaicy <43501017+wivinadaicy@users.noreply.github.com>
2019-07-08 22:29:58 +0700

    laporan quotation beres

--- application/models/M_pdf_quotation.php
+++ application/models/M_pdf_quotation.php
@@ -1,5 +1,16 @@
 <?php
 class M_pdf_quotation extends CI_Model{
     
+    function selectQuotation($where){
+        return $this->db->get_where("quotation",$where);
+    }
+
+    function selectPerusahaan($where){
+        return $this->db->query("select * from price_request join quotation on price_request.id_request = quotation.id_request join perusahaan on perusahaan.id_perusahaan = price_request.id_perusahaan WHERE quotation.id_submit_quotation = " . $where['id_submit_quotation']);
+    }
+
+    function selectBarang($where){
+        return $this->db->query("select * from quotation join quotation_item on quotation.id_submit_quotation = quotation_item.id_submit_quotation WHERE quotation.id_submit_quotation = " . $where['id_submit_quotation']);
+    }
 }
 ?>
