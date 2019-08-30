96921a3b6beb08f60e36ea430f3be5848f52a4eb
joshuanatan <joshuanatan.jn@gmail.com>
2019-08-30 08:33:44 +0700

    kurang hari ke tanggal

--- application/helpers/time_helper.php
+++ application/helpers/time_helper.php
@@ -12,4 +12,11 @@ if ( ! function_exists('tambahHariKeTanggal')){
         return $after;
     }   
 }
+if ( ! function_exists('kurangHariKeTanggal')){
+    function kurangHariKeTanggal($tanggal,$hari,$satuan){
+        
+        $after = date("Y-m-d",strtotime($tanggal."-".$hari.$satuan));
+        return $after;
+    }   
+}
 ?>
